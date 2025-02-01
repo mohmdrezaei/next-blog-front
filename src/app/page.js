8e76fda996961714992a27d822036af8fc3f9916
mohmdrezaei <mhwmdrz9@gmail.com>
2025-02-01 23:05:29 +0330

    Show link based on user login

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,33 @@
+"use client"
 
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [username ,  setUsername] = useState("")
+  useEffect(()=>{
+    fetch("http://localhost:6500/profile" , {
+      credentials : "include"
+    }).then(res=> res.json()).then(data=>{
+      setUsername(data.username)
+    })
+  } ,[])
   return (
    <main className="p-2.5">
     <header className="flex justify-between mb-12">
       <a href="#" className="font-bold">MyBlog</a>
-      <nav className="flex gap-3">
-      <a href="#">Login</a>
-      <a href="#">Register</a>
+      <nav className="flex gap-3 font-bold">
+      {username ? (
+       <>
+       <Link href="/create">Create New Post</Link>
+       <a onClick={logoutHandler}>logout</a>
+       </>
+      ) : (
+        <>
+        <Link href="/login">Login</Link>
+        <Link href="/register">Register</Link>
+        </>
+      )}
       </nav>
     </header>
     <div>
